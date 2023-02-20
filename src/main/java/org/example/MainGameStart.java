542f5a7b3b87be9184f39a8ea42be5a526d7a7f5
Naellu <119831581+Naellu@users.noreply.github.com>
2023-02-20 19:42:08 +0900

    [feat] 프로그램을 실행하는 메인 애플리케이션

---
+++ src/main/java/org/example/MainGameStart.java
@@ -0,0 +1,10 @@
+package org.example;
+
+import java.io.IOException;
+
+public class MainGameStart {
+    public static void main(String[] args) throws IOException {
+        RunAndReplay game = new RunAndReplay();
+        game.run();
+    }
+}
